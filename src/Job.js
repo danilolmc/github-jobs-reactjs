60b6fd7f82dd7bcf60deba67604ca5d72d517f9a
DaniloLima122 <limadanilo06@gmail.com>
2020-07-24 17:43:00 -0300

    Finalizado layout dos cards de apresentação das vagas de emprego

--- src/Job.js
+++ src/Job.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Card } from "react-bootstrap";
+import { Card, Badge, Button, Collapse } from "react-bootstrap";
+
+import ReactMarkdown from "react-markdown";
 
 export default function Job({ job }) {
+
+    const [open, setOpen] = useState(false)
     return (
-        <Card>
+        <Card className="my-2">
             <Card.Body>
                 <div className="d-flex justify-content-between">
                     <div>
@@ -12,9 +16,34 @@ export default function Job({ job }) {
                             {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
 
                         </Card.Title>
+
+                        <Card.Subtitle clasName="text-muted mb-2">
+                            {new Date(job.created_at).toLocaleDateString()}
+                        </Card.Subtitle>
+                        <Badge variant="secondary" className="mr-2">{job.type}</Badge>
+                        <Badge variant="secondary">{job.location}</Badge>
                     </div>
 
+                    <img className="d-none d-md-block" height="40" alt={job.company} src={job.company_logo} />
+
                 </div>
+                <div>
+                    <ReactMarkdown source={job.how_to_apply} />
+                </div>
+
+                <Card.Text>
+                    <Button variant="primary" 
+                    
+                        onClick={() => setOpen(!open)}>
+                        {open ? "Esconder detalhes" : "Ver detalhes" }
+                    </Button>
+                </Card.Text>
+                <Collapse in={open}>
+                    <div className="mt-4">
+                        <ReactMarkdown source={job.description} />
+                    </div>
+                </Collapse>
+
             </Card.Body>
         </Card>
     )
