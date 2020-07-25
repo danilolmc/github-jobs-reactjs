ceb5814c2a1980f69d8ed75d3007a28df9b0e6e6
DaniloLima122 <limadanilo06@gmail.com>
2020-07-25 13:24:26 -0300

    Finalizado componente de pesquisa de usuarios"

--- src/userFetchJobs.js
+++ src/userFetchJobs.js
@@ -5,7 +5,8 @@ import axios from "axios";
 const ACTIONS = {
     MAKE_REQUEST: "make-request",
     GET_DATA: "get-data",
-    ERROR: "error"
+    ERROR: "error",
+    UPDATE_HAS_NEXT_PAGE: "update-has-next-page"
 }
 
 const BASE_URL = "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
@@ -22,6 +23,10 @@ function reducer(state, action) {
 
         case ACTIONS.ERROR:
             return { ...state, loading: false, error: action.payload.error }
+        
+            
+        case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+            return { ...state, hasNextPage: action.payload.hasNextPage}
 
         default:
             return state;
@@ -37,15 +42,34 @@ export default function useFetchJobs(params, page) {
 
     useEffect(() => {
 
-        const cancelToken = axios.CancelToken.source()
-
+        const cancelToken1 = axios.CancelToken.source()
+        
         dispatch({ type: ACTIONS.MAKE_REQUEST })
         axios.get(BASE_URL, {
-            cancelToken: cancelToken.token,
+            cancelToken: cancelToken1.token,
             params: { markdown: true, page: page, ...params }
         }).then(res => {
 
             dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } })
+            
+        }).catch(e => {
+            if (axios.isCancel(e)) return
+
+            dispatch({ type: ACTIONS.ERROR, payload: { error: e } })
+            console.log(e.message)
+        })
+
+        const cancelToken2 = axios.CancelToken.source();
+        
+        
+        axios.get(BASE_URL, {
+            cancelToken: cancelToken2.token,
+            params: { markdown: true, page: page + 1, ...params }
+        }).then(res => {
+
+            dispatch({ type: ACTIONS.UPDATE_HAS_NEXT_PAGE, payload: { 
+                
+                hasNextPage: res.data.lenght !== 0 } })
 
         }).catch(e => {
             if (axios.isCancel(e)) return
@@ -54,9 +78,11 @@ export default function useFetchJobs(params, page) {
             console.log(e.message)
         })
 
+
         return () => {
 
-            cancelToken.cancel()
+            cancelToken1.cancel();
+            cancelToken2.cancel();
         }
 
     }, [params, page])
