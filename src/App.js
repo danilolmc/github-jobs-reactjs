f4173bb40b2a8de3da5ebb16835ace58660091dc
DaniloLima122 <limadanilo06@gmail.com>
2020-07-25 12:13:49 -0300

    Adicionando componente de paginação

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import useFetchJobs from "./userFetchJobs";
 
-import {Container} from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 import Job from "./Job";
 
+import JobsPagination from "./JobsPagination";
+
 function App() {
 
-  const [params,setParams] = useState({})
-  const [page,setPage] = useState(1)
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
 
-  const { jobs, loading, error } = useFetchJobs(params,page);
+  const { jobs, loading, error } = useFetchJobs(params, page);
 
   return (
 
-      <Container>
-          {loading && <h1>Loading...</h1>}
-          {error && <h1>error</h1>}
-          {jobs.map(job =>(
+    <Container className="my-4">
+      <h1 className="mb-4">GitHub Empregos </h1>
+      <JobsPagination page={page} setPage={setPage} hasNextPage={true} />
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>error</h1>}
+        {jobs.map(job => (
 
-            <Job key={job.id} job={job}/>
+          <Job key={job.id} job={job} />
 
-          ))}
-      </Container>
+        ))}
+        <JobsPagination page={page} setPage={setPage} hasNextPage={true} />
+    </Container>
   );
 }
 
