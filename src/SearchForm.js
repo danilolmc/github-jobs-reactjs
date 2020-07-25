07b6b56841d8faf254e4b339e4d91145535562b0
DaniloLima122 <limadanilo06@gmail.com>
2020-07-25 13:25:29 -0300

    Adicionado o formulario de pesquisa

---
+++ src/SearchForm.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import {Form,Col} from "react-bootstrap";
+
+export default function SearchForm({params,onParamChange}) {
+
+    return (
+        <Form className="mb-4">
+            <Form.Row className="align-items-end">
+                <Form.Group as={Col}>                    
+                
+                <Form.Label>Vaga</Form.Label>
+                    <Form.Control onChange={onParamChange} value={params.description} name="description"/>
+                </Form.Group>    
+
+                <Form.Group as={Col}>
+                    <Form.Label>Localização</Form.Label>
+                    <Form.Control onChange={onParamChange} value={params.description} name="location"/>
+                </Form.Group>    
+                <Form.Group as={Col} xs="auto" className="ml-2">
+                    <Form.Check onChange={onParamChange} value={params.full_time} name="full_time" id="full-time" label="Apenas Integral" type="checkbox" className="mb-2"/>
+                </Form.Group>    
+            </Form.Row>        
+        </Form>
+    )
+}
